8efd9c8fae1b1061c315c8f532ee5a6ef407f594
noelardau <noelardau@protonmail.com>
2025-11-10 17:28:56 +0300

    Update PostgreSQL connection string

--- infrastructure/db/dg.go
+++ infrastructure/db/dg.go
@@ -7,7 +7,7 @@ import (
 
 func CreateNewPgxConnexionPool() (*pgxpool.Pool, error) {
 
-	dbpool, err := pgxpool.New(context.Background(), "postgresql://postgres:gF7dYGWDK9tOUzCN@db.bbfsckuzadzzsdymgzmj.supabase.co:5432/postgres")
+	dbpool, err := pgxpool.New(context.Background(), "postgresql://postgres.bbfsckuzadzzsdymgzmj:gF7dYGWDK9tOUzCN@aws-1-eu-west-1.pooler.supabase.com:5432/postgres")
 	if err != nil {
 		return nil, err
 	}
